a5485efbccfed20128badbcdd2aeb5e44363268a
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-04-10 01:39:26 +0200

    commitTest

--- src/index.js
+++ src/index.js
@@ -9,11 +9,18 @@ import markupSearchPictures from './templates/pictures-list.hbs';
 const ref = {
   submitEl: document.querySelector('#search-form'),
   picturesGallery: document.querySelector('.gallery'),
+  btnLoadMore: document.querySelector('.load-more'),
 };
 
 ref.submitEl = addEventListener('submit', inputSearch);
+ref.btnLoadMore = addEventListener('click', loadMore);
 //--
 const jsonPlaceholderApi = new jsonPlaceholderAPI();
+//--
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 //--
 
 function inputSearch(e) {
@@ -22,6 +29,7 @@ function inputSearch(e) {
   jsonPlaceholderApi.searchName = searchName;
   console.log(searchName);
   //--
+  jsonPlaceholderApi.page = 1;
   jsonPlaceholderApi
     .fetchPictures()
     .then(({ data }) => {
@@ -36,14 +44,36 @@ function inputSearch(e) {
         );
         return;
       }
-
-      ref.picturesGallery.innerHTML = markupSearchPictures(data.hits);
+      ref.picturesGallery.innerHTML = '';
+      ref.picturesGallery.insertAdjacentHTML(
+        'beforeend',
+        markupSearchPictures(data.hits)
+      );
       console.log(data);
       console.log();
+      lightbox.refresh();
+
+      console.log(jsonPlaceholderApi.page);
     })
-    .catch(err => {});
+    .catch(err => {
+      console.log(err);
+    });
 }
-var lightbox = new SimpleLightbox('.gallery a', {
-  // captionsData: 'alt',
-  captionDelay: 250,
-});
+
+function loadMore() {
+  jsonPlaceholderApi.page += 1;
+  jsonPlaceholderApi
+    .fetchPictures()
+    .then(({ data }) => {
+      ref.picturesGallery.insertAdjacentHTML(
+        'beforeend',
+        markupSearchPictures(data.hits)
+      );
+      console.log(data);
+      console.log();
+      lightbox.refresh();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
